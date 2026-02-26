ef473e9799fd9d95ac88c18c0b65b4bc767bf47e
kiihne-noaa <Avery.Kiihne@saic.com>
2026-02-25 19:01:21 -0500

    Update epmt_query.py

--- src/epmt/epmt_query.py
+++ src/epmt/epmt_query.py
@@ -1357,7 +1357,7 @@ enabled: boolean, optional
         return r.id
     r_dict = orm_to_dict(r, with_collections=True)
     return pd.Series(r_dict) if fmt == 'pandas' else r_dict
-'''
+
 # returns the number of models deleted.
 def save_refmodel(ReferenceModel, jobs, name=None, tag={}, op_tags=[], computed=computed, info_dict = info_dict, enabled=enabled):
     r = orm_create(ReferenceModel, jobs=jobs, name=name, tags=tag, op_tags=op_tags, computed=computed, info_dict = info_dict, enabled=enabled)
@@ -1368,7 +1368,7 @@ def save_refmodel(ReferenceModel, jobs, name=None, tag={}, op_tags=[], computed=
         return r.id
     r_dict = orm_to_dict(r, with_collections=True)
     return pd.Series(r_dict) if fmt == 'pandas' else r_dict
-'''
+
 @db_session
 def delete_refmodels(*ref_ids):
     """
