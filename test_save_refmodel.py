aca0802fc1415673570b6fb7897cfabd910d1d97
kiihne-noaa <Avery.Kiihne@saic.com>
2026-03-06 11:25:29 -0500

    Update test_save_refmodel.py

--- test_save_refmodel.py
+++ test_save_refmodel.py
@@ -11,9 +11,13 @@ import pandas
 from pandas import DataFrame
 
 feature_list = [ 'duration', 'rchar', 'syscr', 'syscw', 'wchar', 'cstime', 'cutime', 'majflt', 'cpu_time', 'minflt', 'rssmax', 'cmajflt','cminflt', 'inblock', 'outblock', 'usertime', 'num_procs', 'starttime', 'vol_ctxsw', 'read_bytes', 'systemtime', 'time_oncpu', 'timeslices', 'invol_ctxsw', 'write_bytes', 'time_waiting', 'cancelled_write_bytes']
-
 random_jobs =  eq.get_jobs(limit = 100, fmt = 'dict', trigger_post_process=False)
 
+try:
+    r = eq.get_refmodels("bronx_test_model")
+    eq.delete_refmodels(r[0]['id'])
+finally:
+    print("ready for new bronx model")
 r = eq.create_refmodel(random_jobs, methods=es.mvod_classifiers(), features = feature_list, name = "bronx_test_model")
 
 eq.save_refmodel(ReferenceModel, r['jobs'], r['computed'], r['info_dict'], r['enabled'], name='test_name', tag={}, op_tags=[])
