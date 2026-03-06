3135e6d90bf2b63298e6304b0e2431911176fb56
kiihne-noaa <Avery.Kiihne@saic.com>
2026-03-06 11:15:36 -0500

    Create test_save_refmodel.py

---
+++ test_save_refmodel.py
@@ -0,0 +1,19 @@
+import numpy as np
+import epmt
+from epmt import epmt_query
+# epmt_outliers contains the EPMT Outlier Detection API
+from epmt import epmt_outliers as eod
+# epmt_stat contains statistical functions
+from epmt import epmt_stat as es
+from epmt import epmt_query as eq
+from epmt import orm as *
+import pandas
+from pandas import DataFrame
+
+feature_list = [ 'duration', 'rchar', 'syscr', 'syscw', 'wchar', 'cstime', 'cutime', 'majflt', 'cpu_time', 'minflt', 'rssmax', 'cmajflt','cminflt', 'inblock', 'outblock', 'usertime', 'num_procs', 'starttime', 'vol_ctxsw', 'read_bytes', 'systemtime', 'time_oncpu', 'timeslices', 'invol_ctxsw', 'write_bytes', 'time_waiting', 'cancelled_write_bytes']
+
+random_jobs =  eq.get_jobs(limit = 100, fmt = 'dict', trigger_post_process=False)
+
+r = eq.create_refmodel(random_jobs, methods=es.mvod_classifiers(), features = feature_list, name = "bronx_test_model")
+
+eq.save_refmodel(ReferenceModel, r['jobs'], r['computed'], r['info_dict'], r['enabled'], name='test_name', tag={}, op_tags=[])
