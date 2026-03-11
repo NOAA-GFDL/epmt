6e8276f1efdea49f642f5f7bb333b41edf8aa7b7
kiihne-noaa <Avery.Kiihne@saic.com>
2026-03-11 16:53:05 -0400

    Update epmt_query.py

--- src/epmt/epmt_query.py
+++ src/epmt/epmt_query.py
@@ -1365,7 +1365,7 @@ enabled: boolean, optional
 # returns the number of models deleted.
 #jobs, computed, infodict, and emabled can all be retrieved from the ReferenceModel object
 def save_refmodel(ReferenceModel, jobs, computed, info_dict, enabled, name=None, tags={}, op_tags=[]):
-    r = orm_create(ReferenceModel, jobs=jobs, computed=computed, info_dict = info_dict, enabled=enabled, name=name, tags=tag, op_tags=op_tags)
+    r = orm_create(ReferenceModel, jobs=jobs, computed=computed, info_dict = info_dict, enabled=enabled, name=name, tags=tags, op_tags=op_tags)
     orm_commit()
     if fmt == 'orm':
         return r
