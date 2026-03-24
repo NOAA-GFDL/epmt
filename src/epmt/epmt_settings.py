e20b2ab494fab371d2ceec3e00f3506d52d1b784
Ian L. <6273252+ilaflott@users.noreply.github.com>
2026-03-24 18:01:54 -0400

    tweaks (#204)
    * Update pylint thresholds in runtests script
    
    * Update pylint threshold to 8.2
    
    * Update pylint thresholds for epmt module and ui
    
    * Refactor settings import to remove error handling
    
    Simplified the import of user-specific settings by removing the try-except block.
    
    * Remove find_diffs_in_envs function
    
    Removed the find_diffs_in_envs function from epmt_cmds.py. it's not covered, and it's not called, and everything still works! so it must not be needed.

--- src/epmt/epmt_settings.py
+++ src/epmt/epmt_settings.py
@@ -6,8 +6,4 @@ from epmt.epmt_default_settings import *
 
 # now load the user-specific settings.py so they override the defaults
 # if you want your own configuraions, put it in settings.py, not here
-try:
-    from epmt.settings import *
-except Exception as e:
-    raise ModuleNotFoundError('alternate epmt.settings import approach did not' +
-                              ' work and neither did the first attempt!') from e
+from epmt.settings import *
