eb1d87c98ea510e310f24b44da7ad230faa4064c
Ian L. <6273252+ilaflott@users.noreply.github.com>
2026-06-25 11:09:04 -0400

    Update epmtlib.py

--- src/epmt/epmtlib.py
+++ src/epmt/epmtlib.py
@@ -23,8 +23,8 @@ logger = getLogger(__name__)
 # third element is the patch or bugfix number
 # Since we are saving as a tuple you can do a simple
 # compare of two version tuples and python will do the right thing
-_version = (5, 0, 0)
-__version__ = ".".join([str(i) for i in _version]) + ".post"
+_version = (5, 0, 1)
+__version__ = ".".join([str(i) for i in _version]) # + ".post"
 
 
 def version():
